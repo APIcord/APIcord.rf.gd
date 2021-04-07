2cd69f324e67fcfa9fac894190921eec1b6290ea
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-07 11:04:22 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include "includes/header.html" ?>
+    <h2 class="title"><a href="index.html"><img src="https://apicord.github.io/img/apicord.jpeg" alt="Logo"></a> APIcord</h2>
+    <hr>
+    <p>Hugs, memes & maybe a cake</p>
+    <h5><a href="#discord">Discord Edition</a> - <a href="#telegram">Telegram Edition</a> - <a href="credits.html">Credits</a></h5>
+    <br>
+    <div class="sub-stuff" id="discord">
+      <h3>Discord</h3>
+      <h5><a href="https://apicord.github.io/invite">Invite to your server</a> - <a href="https://github.com/APIcord/APIcord">Source Code</a> - <a href="https://gitlab.com/APIcord/APIcord">Mirror</a> - <a href="https://gitlab.com/APIcord/discord/blob/master/README.md">README.md</a></h5>
+      <h6>Functions</h6>
+      <ul>
+        <li>Random Reddit memes</li>
+        <li>Random Images</li>
+        <li>Simple meme generator</li>
+        <li>And more!!!</li>
+      </ul>
+      <h6>Invite</h6>
+      <p>You can invite and vote this bot from...</p>
+      <ul>
+        <li><a href="https://discordlistology.com/bots/757258298725630008">Discordlistology</a></li>
+        <li><a href="https://discord.bots.gg/bots/757258298725630008">Discord Bots (bots.gg)</a></li>
+        <li><a href="https://top.gg/bot/757258298725630008">Discord Bot List / Top.gg / DBL</a></li>
+        <li><a href="https://infinitybotlist.com/bots/757258298725630008">Infinity Bot List</a></li>
+        <li><a href="https://apicord.gitlab.io/invite">This website</a></li>
+      </ul>
+    </div>
+    <br>
+    <div class="sub-stuff" id="telegram">
+      <h3>Telegram</h3>
+      <h5><!--<a href="https://t.me/apicordbot">Test it</a> - <a href="https://github.com/APIcord/telegram">Source Code</a> - <a href="https://gitlab.com/APIcord/telegram">Mirror</a> - <a href="https://github.com/APIcord/telegram/blob/master/README.md">README.md</a></h5> -->
+      <p>Coming soon</p>
+    </div><?php include "includes/footer.html" ?>
